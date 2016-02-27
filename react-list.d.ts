c1ddda295bc86a977f631eefb88fe577c5122085
Kensuke Hoshikawa <Kensuke.Hoshikawa@so-net.co.jp>
2016-02-27 13:39:29 +0900

    add optional.

--- react-list.d.ts
+++ react-list.d.ts
@@ -4,17 +4,17 @@ declare module "react-list" {
   import * as React from 'react';
 
   interface IReactListProps extends React.Props<ReactList> {
-    axis: any;
-    initialIndex: Number;
-    itemSizeGetter: Function;
-    itemRenderer: Function;
-    itemsRenderer: Function;
-    length: Number;
-    pageSize:Number;
-    scrollParentGetter: Function;
-    threshold: Number;
-    type: any;
-    useTranslate3d: Boolean
+    axis?: any;
+    initialIndex?: Number;
+    itemSizeGetter?: Function;
+    itemRenderer?: Function;
+    itemsRenderer?: Function;
+    length?: Number;
+    pageSize?:Number;
+    scrollParentGetter?: Function;
+    threshold?: Number;
+    type?: any;
+    useTranslate3d?: Boolean
   }
 
   interface ReactList extends React.ComponentClass<IReactListProps> {
